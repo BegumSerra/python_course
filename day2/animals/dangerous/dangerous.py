991bda6d3a318d12ce189532f81deeca53a17c5d
BegumSerra <begumserra.buyuktarakci@icm.uu.se>
2022-02-22 15:39:19 +0100

    day2 update2

--- day2/animals/dangerous/dangerous.py
+++ day2/animals/dangerous/dangerous.py
@@ -7,12 +7,3 @@ class Dangerous:
 		print('Printing members of the Dangerous class')
 		for member in self.members:
 			print('\t%s ' % member)
-class Fish:
-        def __init__(self):
-                ''' Constructor for this class. '''
-                # Create some member animals
-                self.members = ['Goldfish', 'Shark', 'Zeprafish']
-        def printMembers(self):
-                print('Printing members of the Fish class')
-                for member in self.members:
-                        print('\t%s ' % member)
